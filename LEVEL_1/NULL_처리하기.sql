837fcdcc440f5b53208396a685bc8a024d0c3526
Kimminsu <alstn2468_@naver.com>
2020-03-16 16:04:51 +0900

    Solve programmers sql problem level 1 NULL 처리하기

---
+++ LEVEL_1/NULL_처리하기.sql
@@ -0,0 +1,9 @@
+-- https://programmers.co.kr/learn/courses/30/lessons/59410
+-- NULL 처리하기
+-- 분류 : IS NULL
+-- 단계 : LEVEL 1
+SELECT
+    ANIMAL_TYPE,
+    IFNULL(NAME, "No name"),
+    SEX_UPON_INTAKE
+FROM ANIMAL_INS;
